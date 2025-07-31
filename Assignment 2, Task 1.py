5c9168bc3b6b5a5e1dfd50b343554f9dc7ff75cd
ArupScript <arupm22@gmail.com>
2025-07-31 22:35:21 +0530

    Add files via upload

---
+++ Assignment 2, Task 1.py
@@ -0,0 +1,6 @@
+#Task 1
+a=int(input("Enter a number: "))
+if a % 2 == 0:
+    print(a , " is a even number")
+else:
+    print(a , " is a odd number")
---
+++ Assignment 2, Task 2.py
@@ -0,0 +1,7 @@
+#Task 2
+total = 0
+
+for number in range(1, 51):
+    total = total + number
+
+print("The sum of numbers from 1 to 50 is:", total)
